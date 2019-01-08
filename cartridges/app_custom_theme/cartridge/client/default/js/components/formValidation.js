7e95e1cd2200305d0cb4f83302b081dd3598c339
Esteban Rios <esteban2web@gmail.com>
2019-01-08 23:17:01 +0100

    Module: update formValidation.js to adapt structure formGroup

--- cartridges/app_custom_theme/cartridge/client/default/js/components/formValidation.js
+++ cartridges/app_custom_theme/cartridge/client/default/js/components/formValidation.js
@@ -18,8 +18,8 @@ module.exports = function (formElement, payload) {
         Object.keys(payload.fields).forEach(function (key) {
             if (payload.fields[key]) {
                 var feedbackElement = $(formElement).find('[name="' + key + '"]')
-                    .parent()
-                    .children('.invalid-feedback');
+                    .closest('.form-group')
+                    .find('.invalid-feedback');
 
                 if (feedbackElement.length > 0) {
                     if (Array.isArray(payload[key])) {
