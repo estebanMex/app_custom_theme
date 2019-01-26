ccb4a41c56b0f27c06b938fe59224ea949e48db2
Esteban Rios <esteban2web@gmail.com>
2019-01-26 23:34:56 +0100

    update path generate snippets

--- bin/generateSnippets.js
+++ bin/generateSnippets.js
@@ -1,6 +1,7 @@
 const Generatorsnippets = require('generatorsnippets');
 // const modulesFilePath = 'PATH_TO/cartridge/templates/default/util/modules.isml';
-const modulesFilePath = '/home/esteban/www-dev/theme_sfra/app_custom_theme/cartridges/app_custom_theme/cartridge/templates/default/components/modules.isml';
+// const modulesFilePath = '/home/esteban/www-dev/theme_sfra/app_custom_theme/cartridges/app_custom_theme/cartridge/templates/default/components/modules.isml';
+const modulesFilePath = '/home/esteban/www-dev/loud-back/app_custom_loud/cartridges/app_custom_loud/cartridge/templates/default/components/modules.isml';
 // Example 1
 new Generatorsnippets({
     'modulesFilePath': modulesFilePath,
